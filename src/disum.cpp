185921fead57d451b0bc2cfc63fc53b4a40aff5e
Brendan Leber <brendan.leber@isilon.com>
2017-07-12 15:11:21 -0700

    Updated layout and setup CMake.
    I've decided to give using CMake a go as there are lots of tools and
    libraries that use it.  This commit also moves the layout of many
    files to a more standard pattern.

---
+++ src/disum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "um.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "Hello Bound Variable from " << argv[0] << "!\n";
+
+    return 0;
+}
--- src/um.cpp
+++ src/um.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-#include <cstdlib>
-
-#include "utils.cpp"
-
-int main(int argc, char** argv)
-{
-	if (argc != 2) {
-		std::cerr << "Syntax: " << argv[0] << " <input.um>\n";
-		return EXIT_FAILURE;
-	}
-
-	auto scroll = load(argv[1]);
-
-	return 0;
-}
+#include "um.hpp"
